32b56d67348e92522cba2429d06728cebc001ef0
Mateusz <33332814+MattaKacz@users.noreply.github.com>
2025-04-23 20:31:15 +0800

    add task functionality

---
+++ todo/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Task(models.Model):
+    task = models.CharField(max_length=250)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.task 
+    
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from todo.models import Task
+
 
 def home(request):
     """
     View function for the home page of the site.
     """
-    return render(request, 'home.html')
+    tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'home.html', context)
