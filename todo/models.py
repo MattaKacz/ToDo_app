95cf9ee279ae6b2c296964a1187db29a886aed64
Mateusz <33332814+MattaKacz@users.noreply.github.com>
2025-04-28 17:52:58 +0800

    todo app features update - delete, edit, mark as done

--- todo/models.py
+++ todo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Task(models.Model):
     task = models.CharField(max_length=250)
     is_completed = models.BooleanField(default=False)
@@ -8,5 +9,4 @@ class Task(models.Model):
     due_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return self.task 
-    
+        return self.task
--- todo_main/views.py
+++ todo_main/views.py
@@ -8,7 +8,10 @@ def home(request):
     View function for the home page of the site.
     """
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
     context = {
         'tasks': tasks,
+        'completed_tasks': completed_tasks,
     }
     return render(request, 'home.html', context)
